a95c006b6ffad1674e45c358537b506685762610
igorsatsyuk <igor.satsyuk2@gmail.com>
2026-03-15 14:53:18 +0200

    chore: start Spring Boot 4 PoC

--- src/main/java/lt/satsyuk/config/RestTemplateConfig.java
+++ src/main/java/lt/satsyuk/config/RestTemplateConfig.java
@@ -1,7 +1,6 @@
 package lt.satsyuk.config;
 
-import org.springframework.boot.actuate.metrics.web.client.ObservationRestTemplateCustomizer;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +9,9 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder, ObservationRestTemplateCustomizer customizer) {
-        return builder.additionalCustomizers(customizer).build();
+    public RestTemplate restTemplate(ObservationRegistry observationRegistry) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setObservationRegistry(observationRegistry);
+        return restTemplate;
     }
 }
